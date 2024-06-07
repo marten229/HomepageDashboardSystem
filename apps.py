bec8dc9bbdcfd711cf565c05dc05b245832d606d
Marten Schulz <ms@marten-schulz.de>
2024-06-07 11:57:29 +0200

    Initial Commit

---
+++ apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HomepagedashboardsystemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'HomepageDashboardSystem'
