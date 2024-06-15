7fbaf4cc95a21edcfa852d58a593cfe5c28d9097
Marten Schulz <ms@marten-schulz.de>
2024-06-15 16:26:42 +0200

    Integration vom Dashboard

--- views.py
+++ views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from RestaurantManagement.models import Restaurant
 
 def home(request):
     restaurants = Restaurant.objects.all()
     return render(request, 'home.html', {'restaurants': restaurants})
+
+
+def dashboard_list(request):
+    return render(request, 'dashboard_list.html')
+
+def dashboard_detail(request, pk):
+    restaurant = get_object_or_404(Restaurant, pk=pk)
+    return render(request, 'dashboard_detail.html', {'restaurant': restaurant})
